063ccfe585d76161225693fcb9f7a2ece48234eb
Mateusz Górzyński <mateusz.gorzynski@gmail.com>
2021-03-08 19:03:54 +0100

    Create new frontend for new API

--- en/script.js
+++ en/script.js
@@ -1,17 +1,11 @@
-const getRowHTML = (user, time) => {
-    return `<tr><td scope=\"row\">${user}</td><td>${time}</td></tr><tr>`;
-}
-const getCardHTML = (id, username, ) => {
+const createCardHTML = (id, username,) => {
     return `<div class="col-md-6"> <div class="card mt-3 " id="shares-card-${id}">
     <div class="card-header">
         ${username}
     </div>
     <div class="card-body">
-        <h4 class="card-title" id="shares_${id}"></h4>
+        <h4 class="card-title" id="shares_${id}">-</h4>
         total shares
-    </div>
-    <div class="card-footer text-muted">
-        <a href="javascript:updateTitle(${id})">show in title</a>
     </div>
 </div></div>`;
 }
@@ -19,117 +13,60 @@ const getCardHTML = (id, username, ) => {
 const toggleMode = () => {
     const element = document.body;
     element.classList.toggle("dark-mode");
-    let darkModeCookieValue = element.classList.contains("dark-mode");
+    const darkModeCookieValue = element.classList.contains("dark-mode");
     localStorage.setItem("dark-mode", darkModeCookieValue);
-  }
-  
-
-const updateTitleOnce = (id) => {
-    let all = 0;
-    counts.forEach((item,index) => {
-            all += item;
-    });
-    document.title = `${counts[id]}/${all} - kopalnia` ;
-    
 }
 
-const updateTitle  = (id) => {    
-    updateTitleOnce(id);
-    setInterval(updateTitleOnce, 5000, id);
+const getUsers = async (users, errorDiv) => {
+    return fetch('https://mining.mateu.us/api/users').then(response => {
+        errorDiv.classList.add('d-none')
+        return response.json()
+    }).catch(err => {
+        errorDiv.classList.remove('d-none')
+        return users
+    })
 }
 
-const testJsonAll = `{"users":[{"id":1,"name":"Mateusz"},{"id":2,"name":"Piotr"}],"shares":[]}`;
-const testJsonUserShares = `[]`;
-
-let userListElement = document.getElementById("user-list");
-let tableElement = document.getElementById("table-body-shares");
-
-let users = [];
-let counts = [];
-
-const getUsers = async () => {
-    const response = await fetch(`/api/all`);
-    const myJson = await response.json(); 
-    users = myJson.users;
-}
-
-const createCardsForUsers = () => {
+const createCardsForUsers = (users, userListDiv) => {
+    userListDiv.innerHTML = ''
     users.forEach((user) => {
-        userListElement.innerHTML += getCardHTML(user.id, user.name);
+        userListDiv.innerHTML += createCardHTML(user.id, user.name);
     });
 }
 
-const getShareCounts = async () => {
-    users.forEach(async e => {
-        const response = await fetch(`/api/user-shares?id=${e.id}`);
-        const myJson = await response.json(); 
-        const element = document.getElementById(`shares_${e.id}`);
-        counts[e.id] = myJson.length;
-        element.innerHTML = counts[e.id];
-    });
+const updateShareCounts = async (users) => {
+    users.forEach(async user => {
+        const element = document.getElementById(`shares_${user.id}`)
+        element.innerHTML = (await fetch(`https://mining.mateu.us/api/share-count?id=${user.id}`).then(response => response.json()))
+    })
 }
 
-
-const clearTable = () => {
-    tableElement.innerHTML = "";
-}
-
-
-let tableJson = [];
-
-
-const jsonDiff = (json_new, json_old) => {
-    let toAdd = [];
-    if(json_new.length > tableJson.length) {
-        toAdd += json_new.filter((e, i) =>  {
-            return i >= tableJson.length;
-        });
-    }
-    return toAdd;
-}
-
-
-const getTable = async () => {
-    const response = await fetch(`/api/all`);
-    const myJson = await response.json(); 
-
-    myJson.shares.forEach(element => {
-        tableElement.innerHTML += getRowHTML(element.id, element.date)
-    });
-    
-
-}
-
-const getTableIfDiff = async () => {
-    const response = await fetch(`/api/all`);
-    const myJson = await response.json(); 
-    let diffArr = jsonDiff(myJson.shares, tableJson.shares);
-    diffArr.forEach(element => {
-        tableElement.innerHTML += getRowHTML(element.id, element.date)
-    });
-        tableJson = myJson;
-    
-
-}
-
-if(localStorage.getItem("dark-mode") == "true") {
+if (localStorage.getItem("dark-mode") == "true") {
     document.body.classList.add("dark-mode");
 }
 
-
-const onTick = () => {
-    getShareCounts();
-    getTableIfDiff();
-}
-clearTable();
-getTable().then(
-    () => {
-        getUsers().then(() => {
-            createCardsForUsers();
-            onTick();
-            setInterval(onTick, 15000);
-        });
-
+const sameUsers = (a, b) => {
+    if (a.length != b.length) {
+        return false
     }
-)
+    for (let i = 0; i < a.length; i++) {
+        if (!(a[i].id == b[i].id && a[i].name == b[i].name)) {
+            return false
+        }
+    }
+    return true
+}
 
+window.onload = () => {
+    const updateStats = async (users, errorDiv, userListDiv) => {
+        let newUsers = await getUsers(users, errorDiv)
+        if (!sameUsers(users, newUsers)) {
+            users = newUsers
+            createCardsForUsers(users, userListDiv)
+        }
+        await updateShareCounts(users, errorDiv)
+        setTimeout(updateStats, 2500, users, errorDiv, userListDiv)
+    }
+
+    updateStats([], document.getElementById('net-error'), document.getElementById("user-list"))
+}
